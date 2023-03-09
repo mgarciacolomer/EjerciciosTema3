0b59f39381dcc42110690139a71a9f31e8661600
Marcos <mgarciacolomer@gmail.com>
2023-03-09 10:24:37 +0100

    Ejercicios Tema3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int sumando1 = 1;
-        int sumando2 = 2;
-        int sumando3 = 3;
+        int n1 = 1;
+        int n2 = 2;
+        int n3 = 3;
         int res;
 
-        res = suma(sumando1, sumando2, sumando3);
+        res = suma(n1, n2, n3);
 
         System.out.println(res);
     }
